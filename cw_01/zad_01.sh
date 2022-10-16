9386e8ffe10ee96a1764caada42613158ed59a9b
Denwiliusz27 <danwielgosz@gmail.com>
2022-10-16 13:01:58 +0200

    cw_01 - zad_01 v2.0

--- cw_01/zad_01.sh
+++ cw_01/zad_01.sh
@@ -2,16 +2,20 @@
 #Daniel Wielgosz (g1)
 
 help() {
+  echo
   echo "~~~~~~~~~~~~~~~~~~~~~~~~ OPIS ~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo "~ Skrypt ma za zadanie wyświetlenie na ekranie"
   echo "~ loginu oraz imienia i nazwiska zalogowanego użytkownika"
   echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  echo
 }
 
 get_data() {
+  echo
   echo "Username: "$USER
   FULLNAME=$(getent passwd $USER | cut -d : -f 5 | cut -d, -f1)
-  echo "" $FULLNAME
+  echo "Imie i nazwisko: " $FULLNAME
+  echo
 }
 
 if [ $# -eq 0 ]
@@ -20,7 +24,6 @@ then
 else
 
 # pętla sprawdza czy podano argument '-h' lub '--help'
-  echo "############### sprawdzam help ####################"
   for var in "$@"
   do
     if [ "$var" == "-h" ] || [ "$var" == "--help" ]
@@ -29,35 +32,30 @@ else
       exit 0
     fi
   done
-  echo "###################################################"
 
 # jeśli nie podano argumentu '-h' lub '--help', program sprawdza czy podano '-q' lub '--qiet'
-  echo "############### sprawdzam quiet ####################"
   for var in "$@"
   do
     if [ "$var" == "-q" ] || [ "$var" == "--quiet" ]
     then
-      echo "koncze dzialanie :("
       exit 0
     fi
   done
-  echo "###################################################"
 
 # program sprawdza czy nie podano innych argumentów
-  echo "############### sprawdzam inne ####################"
   for var in "$@"
   do
-    if [ "${var::1}" != "-" ]
+    if [ "${var::1}" == "-" ] || [ "${var::2}" == "--" ]
     then
-      get_data
-      exit 0
-    else
-      echo "ERROR: Nie rozumiem takiej opcji :("
+      echo
+      echo "ERROR: podano niewłaściwą opcję"
       help
       exit 1
     fi
   done
-  echo "###################################################"
+
+  get_data
+  exit 0
 fi
 
 
