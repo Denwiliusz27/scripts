524df79115a7fbb517ccc51bac30c42007286dfa
Denwiliusz27 <danwielgosz@gmail.com>
2023-01-21 15:24:42 +0100

    add perl fiile

--- cw_12/pl_skrypt04.pl
+++ cw_12/pl_skrypt04.pl
@@ -1,70 +1,72 @@
 #!/usr/bin/perl
 #Daniel Wielgosz (g1)
 
+use File::Find;
+my @files;
+
+sub collect_files {
+    my $file = $File::Find::name;
+    push @files, $file if (-f $file);
+}
+
 $wz = 0;
 @temp_paths = [];
 %paths = ();
 %results = ();
 
 for(my $i = 1; $i <= $#ARGV ; $i++) {
-    print "~~~~~~~~~~~~~~~~~~~~~\n";
-    print "arg: @ARGV[$i] \n";
     if ( $ARGV[$i-1] eq '-d') {
-        # if ((-d $ARGV[$i] ) | (-e $ARGV[$i])){
-        #     print "scieżka $key istnieje\n";
-        # } else {
-        #     print "ERROR - ścieżka '$ARGV[$i]' nie istnieje\n";
-        # }
-
-        print "wstawiam $ARGV[$i] do temp: ";
         push (@temp_paths, $ARGV[$i]);
-        print "@temp_paths[1]\n";
 
         if (not(exists($paths{$ARGV[$i]}))){
-            print "wstawiam pusta tablice: ";
             $paths{$ARGV[$i]} = [];
-            print "$paths{$ARGV[$i]} \n"
         }
         if (not(exists($results{$ARGV[$i]}))){
-            print"wstawiam pusty slownik: ";
-            $results{$ARGV[$i]} = ();
-            print "$results{$ARGV[$i]}\n";
+            $results{$ARGV[$i]} = {};
         }
     } elsif ( $ARGV[$i] eq '-d' & $wz == 1) {
-        print"zeruje\n";
         $wz = 0;
         @temp_paths = [];
     } elsif (($ARGV[$i-2] eq '-d' & $ARGV[$i] ne '-d') | $wz == 1){
-        print"wstawiam $ARGV[$i] z temp do slownika: \n";
         $wz = 1;
 
-        for my $key (@temp_paths){
+        for my $key (@temp_paths[1..$#temp_paths]){
             push @{$paths{$key}}, $ARGV[$i];
-            print "$key : @{$paths{$key}} \n";
         }
-
-        # for (@temp_paths[1 .. $#temp_paths]){
-        #     push @{$paths{@temp_path[$_]}}, $ARGV[$i];
-        #     print "@temp_path[$_] : @{$paths{@temp_path[$_]}} \n";
-        # }
     }
 }
 
-foreach my $key (keys %paths) {
-    my $value = $paths{$key};
-    print "Key: $key\n";
-    print "Values: ";
-    foreach my $element (@$value) {
-        print "$element ";
+foreach my $key (keys %paths){
+    if ((-d $key ) | (-e $key)){
+        find(\&collect_files, $key);
+
+        foreach my $file (@files){
+            open(FH, $file);
+            while(<FH>){
+                foreach $word (@{$paths{$key}}){
+                    $count = () = $_ =~ /$word/g;
+
+                    if ($count >= 1){
+                        # print "jest $count : $_";
+                        if (not(exists($results{$key}{$word}))){
+                            $results{$key}{$word} = $count;
+                        } else{
+                            $results{$key}{$word} += $count;
+                        }
+                    }
+                }
+            }
+        }
+        @files = ();
+    } else {
+        print "ERROR - ścieżka '$key' nie istnieje\n";
+    }
+}
+
+while (my ($key, $value) = each %results) {
+    print "$key : ";
+    while (my ($subkey, $subvalue) = each %$value) {
+        print "'$subkey' - $subvalue ";
     }
     print "\n";
 }
-
-# foreach my $key (keys %paths){
-#     if ((-d $key ) | (-e $key)){
-#         print "scieżka $key istnieje\n";
-#     } else {
-#         print "ERROR - ścieżka '$key' nie istnieje\n";
-#     }
-# }
-
--- cw_12/py_skrypt04.py
+++ cw_12/py_skrypt04.py
@@ -3,6 +3,18 @@
 
 import sys, os
 
+
+def count(my_file, paths, key, results):
+    for line in my_file:
+        for word in paths[key]:
+            if str(line).count(str(word)) > 0:
+                # print("jest", str(line).count(str(word)), ": ", line)
+                if word not in results[key]:
+                    results[key][word] = str(line).count(str(word))
+                else:
+                    results[key][word] += str(line).count(str(word))
+
+
 paths = {}
 results = {}
 temp_paths = []
@@ -16,7 +28,7 @@ for i in range(1,len(sys.argv)):
         if sys.argv[i] not in results:
             results[sys.argv[i]] = {}
 
-    elif sys.argv[i] == '-d' and wz:
+    elif (sys.argv[i] == '-d' and wz):
         wz = False
         temp_paths = []
     elif (sys.argv[i-2] == '-d' and sys.argv[i] != '-d') or wz:
@@ -28,20 +40,17 @@ for key in paths:
     if not os.path.exists(key):
         print("ERROR - ścieżka '", key, "' nie istnieje")
         continue
+    if os.path.isfile(key):
+        with open(key, 'rb') as my_file:
+            count(my_file, paths, key, results)
 
     for subdir, dirs, files in os.walk(key):
         for file in files:
             with open(os.path.join(subdir, file), 'rb') as my_file:
-                for line in my_file:
-                    splitted_line = line.split()
-
-                    for word in paths[key]:
-                        if str(line).count(str(word)) > 0:
-                            if word not in results[key]:
-                                results[key][word] = str(line).count(str(word))
-                            else:
-                                results[key][word] += str(line).count(str(word))
+                count(my_file, paths, key, results)
 
 for key in results:
+    print(key, ": ", end = "")
     for u_key in results[key]:
-        print(key, ": '", u_key, "'-", results[key][u_key])
+        print("'", u_key, "'-", results[key][u_key], end =" ")
+    print()
